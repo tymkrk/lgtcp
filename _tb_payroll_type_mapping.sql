74bda30689f4b4131b7e8f15beee2ba4514c6b8d
tymkrk <tymoteusz.kruk@beqom.com>
2025-04-11 15:27:33 +0200

    Create _tb_payroll_type_mapping.sql

---
+++ _tb_payroll_type_mapping.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dbo].[_tb_payroll_type_mapping](
+	[year] [int] NOT NULL,
+	[payroll_type_code] [nvarchar](20) NOT NULL,
+	[target_field_code] [nvarchar](510) NULL,
+	[paid_field_code] [nvarchar](510) NULL,
+	[target_min_field_code] [nvarchar](510) NULL,
+	[target_max_field_code] [nvarchar](510) NULL
+) ON [PRIMARY]
+GO
+
+
