dfad403a9db3730fd4e33ad027447559a9057292
tymkrk <tymoteusz.kruk@beqom.com>
2025-04-18 16:39:32 +0200

    Update insert__tb_payroll_type_mapping.sql

--- insert__tb_payroll_type_mapping.sql
+++ insert__tb_payroll_type_mapping.sql
@@ -1,13 +1,28 @@
+
+declare @script_name_insert_tb_payroll_type_mapping_2024 nvarchar(100) = 'insert_tb_payroll_type_mapping_2024'
+
+if not exists (select top 1 1 from [dbo].[_tb_one_time_scripts_release_log] where script_name = @script_name_insert_tb_payroll_type_mapping_2024)
+begin
+
 	insert into _tb_payroll_type_mapping
 		(year
 		,payroll_type_code
 		,paid_field_code
 		,target_field_code
 		,target_min_field_code
-		,target_max_field_code)
-	select 2024, 1000,'AnnualBaseSalary_CRP-LGT-Base_JLY_2024',null,null,null union
-	select 2024, 2100,'PaidBonus_CRP-LGT-PEP_2024',null,'PaidBonus_CRP-LGT-PEmin_2024','PaidBonus_CRP-LGT-PEmax_2024' union
-	select 2024, 2020,'PaidBonus_CRP-LGT-TBProfStaff_2024','TargetBonus_CRP-LGT-Target_TBProfStaff_2024',null,null union
-	select 2024, 2070,'PaidBonus_CRP-LGT-DiscrBonus_2024',null,null,null union
-	select 2024, 2071,'PaidBonus_CRP-LGT-ExitBonus_2024',null,null,null union
-	select 2024, 2080,'PaidBonus_CRP-LGT-SalesComm_2024',null,null,null
+		,target_max_field_code
+		,plan_id)
+			select	2024,	2020,	NULL											,'PaidBonus_CRP-LGT-TBProfStaff_2024'	,NULL										,NULL									,	13
+	union	select	2024,	2070,	NULL											,'PaidBonus_CRP-LGT-DiscrBonus_2024'	,NULL										,NULL									,	13
+	union	select	2024,	2071,	NULL											,'PaidBonus_CRP-LGT-ExitBonus_2024'		,NULL										,NULL									,	13
+	union	select	2024,	2080,	NULL											,'PaidBonus_CRP-LGT-SalesComm_2024'		,NULL										,NULL									,	13
+	union	select	2024,	2100,	NULL											,'PaidBonus_CRP-LGT-PEP_2024'			,NULL										,NULL									,	13
+	union	select	2025,	2020,	'TargetBonus_CRP-LGT-Target_TBProfStaff_2024'	,NULL									,NULL										,NULL									,	13
+	union	select	2025,	2100,	NULL											,NULL									,'TargetBonus_CRP-LGT-Target_PEmin_2024'	,'TargetBonus_CRP-LGT-Target_PEmax_2024',	13
+	union	select	2024,	2070,	NULL											'PaidBonus_CRP-LGT-GIMPerformance_2024'	,NULL										,NULL									,	13
+
+	insert into [dbo].[_tb_one_time_scripts_release_log]
+		(script_name
+		,applied_on)
+	select @script_name_insert_tb_payroll_type_mapping_2024, GETDATE()
+end
