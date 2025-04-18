eb81c5fdb5771677db43e45e5106808e27373dd7
tymkrk <tymoteusz.kruk@beqom.com>
2025-04-18 16:33:03 +0200

    Update _tb_payroll_type_mapping.sql

--- _tb_payroll_type_mapping.sql
+++ _tb_payroll_type_mapping.sql
@@ -1,10 +1,14 @@
+
+
 CREATE TABLE [dbo].[_tb_payroll_type_mapping](
 	[year] [int] NOT NULL,
 	[payroll_type_code] [nvarchar](20) NOT NULL,
 	[target_field_code] [nvarchar](510) NULL,
 	[paid_field_code] [nvarchar](510) NULL,
 	[target_min_field_code] [nvarchar](510) NULL,
-	[target_max_field_code] [nvarchar](510) NULL
+	[target_max_field_code] [nvarchar](510) NULL,
+	[plan_id] [int] NULL,
+	[Id] [int] IDENTITY(1,1) NOT NULL
 ) ON [PRIMARY]
 GO
 
